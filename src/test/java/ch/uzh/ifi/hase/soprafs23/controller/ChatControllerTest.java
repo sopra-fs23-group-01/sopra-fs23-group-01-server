77f7da872e93fed3d9f476af58d4e4e052347657
Hanky Liu <103504158+zihanltesla@users.noreply.github.com>
2023-05-22 23:33:48 +0200

    Add_chatcontroller_test

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/ChatControllerTest.java
@@ -0,0 +1,124 @@
+package ch.uzh.ifi.hase.soprafs23.controller;
+
+import ch.uzh.ifi.hase.soprafs23.constant.GameStage;
+import ch.uzh.ifi.hase.soprafs23.entity.Room;
+import ch.uzh.ifi.hase.soprafs23.model.Message;
+import ch.uzh.ifi.hase.soprafs23.model.Status;
+import ch.uzh.ifi.hase.soprafs23.service.ChatService;
+import ch.uzh.ifi.hase.soprafs23.service.RoomService;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ChatControllerTest {
+
+    @Autowired
+    private ChatController chatController;
+
+    @MockBean
+    private RoomService roomService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private ChatService chatService;
+
+    @MockBean
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    private Message message;
+    private Long roomId;
+
+    @BeforeEach
+    public void setup() {
+        roomId = 1L;
+        message = new Message();
+        message.setSenderName("test user");
+        message.setStatus(Status.ASSIGNED_WORD);
+    }
+
+    @Test
+    public void testReceiveMessage() {
+        Mockito.when(roomService.assignWord(any())).thenReturn("test word");
+        Mockito.when(roomService.assignSide(any())).thenReturn("test side");
+        
+        chatController.receiveMessage(message, roomId);
+        
+        Mockito.verify(roomService).assignWord(eq(message.getSenderName()));
+        Mockito.verify(roomService).assignSide(eq(message.getSenderName()));
+        Mockito.verify(chatService).systemReminder(eq("test word"), eq(roomId));
+        Mockito.verify(simpMessagingTemplate).convertAndSendToUser(eq(message.getSenderName()), eq("/private"), any(Message.class));
+    }
+
+    @Test
+    public void testReceiveCreationMessage() {
+        chatController.receiveCreationMessage(message);
+        
+        Mockito.verify(simpMessagingTemplate).convertAndSend(eq("/room"), eq(message));
+    }
+
+    @Test
+    public void testRecMessage() {
+        chatController.recMessage(message);
+
+        Mockito.verify(simpMessagingTemplate).convertAndSendToUser(eq(message.getReceiverName()), eq("/private"), eq(message));
+    }
+
+
+    //测试玩家都准备了开始游戏可以正常开始
+    @Test
+    public void startGame_AllPlayersReady() {
+        Room room = new Room();
+        room.setGameStage(GameStage.WAITING);
+        when(roomService.findRoomById(roomId)).thenReturn(room);
+        when(roomService.checkIfAllReady(room)).thenReturn(true);
+    
+        // When conductTurn is called, change gameStage to END.
+        doAnswer(invocation -> {
+            room.setGameStage(GameStage.END);
+            return null;
+        }).when(chatService).conductTurn(room, roomId);
+    
+        chatController.startGame(roomId);
+        
+        //测试是否用到了这些函数
+        verify(chatService).initiateGame(room, roomId);
+        verify(chatService).broadcastGameStart(roomId);
+        verify(chatService).conductTurn(room, roomId);
+        verify(chatService).broadcastGameEnd(room, roomId);
+    }
+
+
+    //测试有玩家没有准备但是游戏开始是否正常
+    @Test
+    public void startGame_NotAllPlayersReady() {
+        Room room = new Room();
+        room.setGameStage(GameStage.WAITING);
+
+        when(roomService.findRoomById(roomId)).thenReturn(room);
+        when(roomService.checkIfAllReady(room)).thenReturn(false);
+
+        chatController.startGame(roomId);
+
+        verify(chatService).systemReminder("Not enough players or not all players are ready yet!",roomId);
+        verify(chatService, never()).initiateGame(any(), any());
+        verify(chatService, never()).broadcastGameStart(any());
+        verify(chatService, never()).conductTurn(any(), any());
+        verify(chatService, never()).broadcastGameEnd(any(), any());
+    }
+}
--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/ChatServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/ChatServiceTest.java
@@ -1,23 +1,26 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
-
 import ch.uzh.ifi.hase.soprafs23.constant.GameStage;
+import ch.uzh.ifi.hase.soprafs23.constant.Role;
 import ch.uzh.ifi.hase.soprafs23.constant.RoomProperty;
 import ch.uzh.ifi.hase.soprafs23.entity.Room;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.model.Message;
+import ch.uzh.ifi.hase.soprafs23.model.Status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-
+import java.util.Set;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import org.awaitility.Awaitility;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 
@@ -33,6 +36,9 @@ public class ChatServiceTest {
     private SimpMessagingTemplate simpMessagingTemplate;
     @Mock
     private ChatService chatService;
+
+    @Captor
+    private ArgumentCaptor<Message> messageCaptor;
 
     @BeforeEach
     public void setup() {
@@ -138,6 +144,127 @@ public class ChatServiceTest {
         when(userService.getUserById(anyLong())).thenReturn(user);
         room.setGameStage(GameStage.VOTING);
         // Invoke the method under test
-        chatService.conductTurn(room, room.getRoomId());
+        chatService.conductTurn(room, room.getRoomId());}
+        
+
+    //测试开始游戏以后发出去的是否和想要发的一样
+    @Test
+    public void testBroadcastGameStart() {
+        // Prepare test data
+        Long roomId = 1L;
+
+        // No need to stub anything as we're testing void method and interaction with mocked dependencies
+
+        // Invoke the method under test
+        chatService.broadcastGameStart(roomId);
+
+        // Capture the argument and verify that the correct message was sent to the correct room
+        verify(simpMessagingTemplate).convertAndSend(eq("/chatroom/"+roomId+"/public"), messageCaptor.capture());
+
+        Message capturedMessage = messageCaptor.getValue();
+        
+        // Now assert that the properties of the message are as expected
+        assertEquals("system", capturedMessage.getSenderName());
+        assertEquals("Game has started!", capturedMessage.getMessage());
+        assertEquals(Status.START, capturedMessage.getStatus());
+    }
+
+    @Test
+    public void testSystemReminder() {
+        // Prepare test data
+        Long roomId = 1L;
+        String reminderInfo = "Test Reminder";
+
+        // No need to stub anything as we're testing void method and interaction with mocked dependencies
+
+        // Invoke the method under test
+        chatService.systemReminder(reminderInfo, roomId);
+
+        // Capture the argument and verify that the correct message was sent to the correct room
+        verify(simpMessagingTemplate).convertAndSend(eq("/chatroom/"+roomId+"/public"), messageCaptor.capture());
+
+        Message capturedMessage = messageCaptor.getValue();
+        
+        // Now assert that the properties of the message are as expected
+        assertEquals("system", capturedMessage.getSenderName());
+        assertEquals(reminderInfo, capturedMessage.getMessage());
+        assertEquals(Status.REMINDER, capturedMessage.getStatus());
+    }
+
+    @Test
+    public void testAssignUserRole() {
+        Set<String> possibleRoles = new HashSet<>(Arrays.asList("detective", "spy"));
+        Set<String> assignedRoles = new HashSet<>();
+
+        // Run the method under test multiple times
+        for (int i = 0; i < 100; i++) {
+            String role = chatService.assignUserRole();
+            // Check that the returned role is either "detective" or "spy"
+            assertTrue(possibleRoles.contains(role));
+            // Keep track of which roles have been returned
+            assignedRoles.add(role);
+        }
+
+        // Check that both "detective" and "spy" have been returned at least once
+        assertEquals(possibleRoles, assignedRoles);
+    }
+
+    @Test
+    public void testDescriptionBroadcast() {
+        // Prepare test data
+        Long roomId = 1L;
+        String userName = "Test User";
+
+        // No need to stub anything as we're testing void method and interaction with mocked dependencies
+
+        // Invoke the method under test
+        chatService.descriptionBroadcast(userName, roomId);
+
+        // Capture the argument and verify that the correct message was sent to the correct room
+        verify(simpMessagingTemplate).convertAndSend(eq("/chatroom/"+roomId+"/public"), messageCaptor.capture());
+
+        Message capturedMessage = messageCaptor.getValue();
+        
+        // Now assert that the properties of the message are as expected
+        assertEquals(userName, capturedMessage.getSenderName());
+        assertEquals("Now it's Player --" + userName + "'s turn to describe", capturedMessage.getMessage());
+        assertEquals(Status.DESCRIPTION, capturedMessage.getStatus());
+    }
+
+    @Test
+    void testBroadcastGameEnd() {
+        Room room = new Room();
+        Long roomId = 1L;
+
+        // set the room object
+        room.setWinner(Role.UNDERCOVER);
+        room.setUndercoverWord("UndercoverWord");
+        room.setDetectiveWord("DetectiveWord");
+
+        chatService.broadcastGameEnd(room, roomId);
+
+        verify(simpMessagingTemplate).convertAndSend(eq("/chatroom/" + roomId + "/public"), messageCaptor.capture());
+
+        Message sentMessage = messageCaptor.getValue();
+        assertEquals(room.getWinner().toString(), sentMessage.getSenderName());
+        assertEquals("Undercover Word:" + room.getUndercoverWord() + "\nDetective Word:" + room.getDetectiveWord(), sentMessage.getMessage());
+        assertEquals(Status.END, sentMessage.getStatus());
+
+        verify(roomService).EndGame(room);
+    }
+
+    
+    @Test
+    public void testBroadcastVoteStart() {
+        Long roomId = 1L;
+
+        chatService.broadcastVoteStart(roomId);
+
+        verify(simpMessagingTemplate).convertAndSend(eq("/chatroom/" + roomId + "/public"), messageCaptor.capture());
+
+        Message sentMessage = messageCaptor.getValue();
+        assertEquals("system", sentMessage.getSenderName());
+        assertEquals("Now it's time to vote!\n You can click avatar to vote", sentMessage.getMessage());
+        assertEquals(Status.VOTE, sentMessage.getStatus());
     }
 }
